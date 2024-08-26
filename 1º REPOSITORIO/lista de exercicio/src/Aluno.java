ec3caccee875717bb2621a8909acf956c5c2df85
cristiano-rgb <cristianoaparecidojp08@gmail.com>
2024-08-26 14:15:48 -0300

    Criacao do App com classe Aluno

---
+++ 1º REPOSITORIO/lista de exercicio/src/Aluno.java
@@ -0,0 +1,57 @@
+public class Aluno {
+    private String nome;
+    private String curso;
+    private String disciplina;
+    private int bimestre;
+    private String mencao;
+
+    public Aluno(String nome,String curso){
+        this.nome= nome;
+        this.curso= curso;
+
+
+        public void setNome(String nome) {
+            
+        }
+
+    }
+    public String getNome(){
+        return this.nome;
+    }
+
+
+    public void setCurso(String curso) {
+            
+    }
+
+    public String getCurso(){
+        return this.curso;
+    }
+
+
+    public void setDisciplina(String disciplina) {
+            
+    }
+
+    public String getDisciplina(){
+        return this.nome;
+    }
+
+
+    public void setBimestre(int bimestre) {
+            
+    }
+    
+    public int getBimestre(){
+        return this.bimestre;
+    }
+
+
+    public void setMencao(String mencao) {
+            
+    }
+
+    public String getMencao(){
+        return this.nome;
+    }
+}
